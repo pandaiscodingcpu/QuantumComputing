07d2528e6bd1257e9108afdd3480d25e1aa2121f
pandaiscodingcpu <pranshugkp2006@gmail.com>
2026-02-19 10:44:00 +0530

    UPDATE: Quantum gates simulation

---
+++ QuantumGates/gates.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Gates:
+    def __init__(self,inp1,inp2):
+        self.inp1 = inp1
+        self.inp2 = inp2
+
+    # single gates
+    # X-Gate
+    def XGate(self):
+        pass
+
+
+
+
